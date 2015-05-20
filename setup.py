7932aee6ad96375b9f6c842c020a124c1975746e
Ryan Pineo <ryanpineo@gmail.com>
2015-05-20 15:51:34 -0400

    add migrations to packages

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ def read(filename):
 
 setup(
     name='djangorestframework-timed-auth-token',
-    version='1.0.1',
+    version='1.0.2',
     description=('An authentication token for djangorestframework that has an expiration date.'),
     long_description=read('README.rst'),
     author='Ryan Pineo',
     author_email='ryanpineo@gmail.com',
     license='MIT',
     url='https://github.com/silverlogic/djangorestframework-timed-auth-token',
-    packages=['timed_auth_token'],
+    packages=['timed_auth_token', 'timed_auth_token.migrations'],
     install_requires=['djangorestframework>=3.1'],
     classifiers=[
         'Framework :: Django',

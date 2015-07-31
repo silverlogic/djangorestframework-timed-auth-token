68687d06748066a4653db999b83c98a6542d25ad
Ryan Pineo <ryanpineo@gmail.com>
2015-07-31 14:55:59 -0400

    bump version to 1.2.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(filename):
 
 setup(
     name='djangorestframework-timed-auth-token',
-    version='1.1.2',
+    version='1.2.0',
     description=('An authentication token for djangorestframework that has an expiration date.'),
     long_description=read('README.rst'),
     author='Ryan Pineo',
